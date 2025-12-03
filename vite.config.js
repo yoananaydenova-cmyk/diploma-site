8d5eda6b5a7f2117a69c37ff1245a256183dfa9d
Yoana Naydenova <y.naydenova@gmail.com>
2025-12-03 21:20:24 +0200

    add flowbite-react

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import flowbiteReact from "flowbite-react/plugin/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react(), flowbiteReact()]
 })
-
