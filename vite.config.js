1568114832e7ecce1c0b3b323f0707dc8cbd24ff
Yoana Naydenova <y.naydenova@gmail.com>
2026-02-23 22:10:50 +0200

    add vite config base atribute

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import flowbiteReact from "flowbite-react/plugin/vite";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), flowbiteReact()]
+  plugins: [react(), flowbiteReact()],
+  base: '/diploma-site/'
 })
