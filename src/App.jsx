74f58d59608f0e47a5ac0ecd689c96845379b03b
Yoana Naydenova <y.naydenova@gmail.com>
2026-02-24 10:30:24 +0200

    fix rauter

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,24 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Nav from './components/Nav';
-import Home from './pages/Home';
-import About from './pages/About';
-import Diploma from './pages/Diploma';
-import Projects from './pages/Projects';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Nav from "./components/Nav";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Diploma from "./pages/Diploma";
+import Projects from "./pages/Projects";
 
 function App() {
   return (
-    <Router>
+    <Router basename="/diploma-site">
       <Nav />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/diploma" element={<Diploma />} />
-        <Route path="/projects" element={<Projects/>} />
-      </Routes>
+      <div className="container mx-auto mt-6">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/diploma" element={<Diploma />} />
+          <Route path="/projects" element={<Projects />} />
+        </Routes>
+      </div>
     </Router>
   );
 }
-
 
 export default App;
