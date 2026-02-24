078c1f47f6c81a4429c3c1e00aceadf9b7312167
Yoana Naydenova <y.naydenova@gmail.com>
2026-02-24 18:10:51 +0200

    add projects data file

---
+++ src/data/projects.js
@@ -0,0 +1,40 @@
+const projectData = [
+  {
+    name: "Презентации",
+    description: "Изготвяне на професионални презентации за академични и бизнес цели.",
+    image: "https://st2.depositphotos.com/3889193/8014/i/450/depositphotos_80147336-Business-teamwork.jpg",
+    details: "Тук можете да видите всички презентации, които съм изготвила, включително академични, маркетингови и проектни презентации."
+  },
+  {
+    name: "Маркетингови анализи",
+    description: "Подробни анализи за пазари, конкуренция и потребителско поведение.",
+    image: "https://images.openai.com/static-rsc-1/IG9gNFZAY7zyr1pVoDPnEXs13RdPvq0GxVOh6UcTeT5mtyvpailP5KkEG6pU4hHpxBFJNLB2zl8wBpqDmUg0J0XCNgi7ujhJNop1wQiB9gHSyKAE26MTxgO1WUTsJdPp01dvYkNRvxz4LcCNhh1_Ow",
+    details: "Включва SWOT анализи, конкурентни проучвания и пазарни стратегии.Включва SWOT анализи, конкурентни проучвания и пазарни стратегии.Включва SWOT анализи, конкурентни проучвания и пазарни стратегии.Включва SWOT анализи, конкурентни проучвания и пазарни стратегии.Включва SWOT анализи, конкурентни проучвания и пазарни стратегии.Включва SWOT анализи, конкурентни проучвания и пазарни стратегии."
+  },
+  {
+    name: "Финансови казуси",
+    description: "Симулации и решения на финансови казуси с аналитичен подход.",
+    image: "https://plus.unsplash.com/premium_photo-1681487767138-ddf2d67b35c1?fm=jpg&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8ZmluYW5jaWFsJTIwY2hhcnR8ZW58MHx8MHx8fDA%3D&ixlib=rb-4.1.0&q=60&w=3000",
+    details: "Примери от учебни казуси, анализи на бюджети и прогнози."
+  },
+  {
+    name: "Доклади",
+    description: "Подготовка на академични и професионални доклади.",
+    image: "https://st2.depositphotos.com/3889193/8014/i/450/depositphotos_80147336-Business-teamwork.jpg",
+    details: "Включва структурирани доклади с графики и визуализации."
+  },
+  {
+    name: "Уебпроекти",
+    description: "Разработка на малки уеб приложения и сайтове.",
+    image: "https://images.unsplash.com/photo-1457305237443-44c3d5a30b89?fm=jpg&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8d2ViJTIwZGV2ZWxvcG1lbnR8ZW58MHx8MHx8fDA%3D&ixlib=rb-4.1.0&q=60&w=3000",
+    details: "React, Vite, Tailwind CSS - примерни приложения и уеб сайтове."
+  },
+  {
+    name: "Клубни инициативи",
+    description: "Работа по проекти и инициативи в училищни и студентски клубове.",
+    image: "https://media.sciencephoto.com/f0/20/17/11/f0201711-800px-wm.jpg",
+    details: "Организиране на събития, сътрудничество и създаване на материали."
+  },
+];
+
+export default projectData;
