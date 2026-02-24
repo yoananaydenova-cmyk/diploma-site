82263c279a03254da820b27ab65f0ead0ac1e39e
Yoana Naydenova <y.naydenova@gmail.com>
2026-02-24 10:31:34 +0200

    add config files

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx,ts,tsx}",
+    "./node_modules/flowbite-react/**/*.js",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,6 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import flowbiteReact from "flowbite-react/plugin/vite";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  plugins: [react(), flowbiteReact()],
-  base: '/diploma-site/'
-})
+  plugins: [react()],
+});
